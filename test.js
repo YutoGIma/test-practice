cfae9ceafe5bd98365e90b96747a1742976aec0d
GimaYuto <yurihanagima@gmail.com>
2022-05-18 21:15:09 +0900

    TypeScript実装と超初歩

---
+++ test.js
@@ -0,0 +1,8 @@
+function sum(a, b) {
+    return a + b;
+}
+function testSum() {
+    var result = sum(1, 2);
+    return result == 3;
+}
+console.log(testSum());
---
+++ test.ts
@@ -0,0 +1,10 @@
+function sum(a:number,b:number){
+    return a + b;
+}
+
+function testSum(){
+    let result = sum(1,2)
+    return result == 3;
+}
+
+console.log(testSum())
